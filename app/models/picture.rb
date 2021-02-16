d8ce893bfcb4099c930f2dc9d20af1b2c23b5fee
unokain <unokain@gmail.com>
2021-02-16 11:20:36 +0900

    課題完成

---
+++ app/models/picture.rb
@@ -0,0 +1,7 @@
+class Picture < ApplicationRecord
+    mount_uploader :image, ImageUploader
+    validates :content, presence: true
+    validates :content, length: { in: 1..140 }
+    belongs_to :user
+    
+end
